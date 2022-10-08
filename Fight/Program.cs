4c7c21122e0dc37840cb2de9592b2ae072634d54
Suren Arakelyan <suren.arakelyan@elev.ga.ntig.se>
2022-10-08 22:13:59 +0200

    first commit

---
+++ Fight/Program.cs
@@ -0,0 +1,45 @@
+﻿
+int heroHp = 100;
+int villainHp = 100;
+
+string heroName = "Riddaren";
+string villainName = "Troll";
+
+Random generator = new Random();
+
+while (heroHp > 0 && villainHp > 0)
+{
+  Console.WriteLine("\n----- ===== NY RUNDA ===== -----");
+  Console.WriteLine($"{heroName}: {heroHp}  {villainName}: {villainHp}\n");
+
+  int heroDamage = generator.Next(30);
+  villainHp -= heroDamage;
+  villainHp = Math.Max(0, villainHp);
+  Console.WriteLine($"{heroName} gör {heroDamage} skada på {villainName}");
+
+  int villainDamage = generator.Next(20);
+  heroHp -= villainDamage;
+  heroHp = Math.Max(0, heroHp);
+  Console.WriteLine($"{villainName} gör {villainDamage} skada på {heroName}");
+
+  Console.WriteLine("Tryck på vilken knapp du vill för att fortsätta.");
+  Console.ReadKey();
+}
+
+Console.WriteLine("\n----- ===== STRIDEN ÄR SLUT ===== -----");
+
+if (heroHp == 0 && villainHp == 0)
+{
+  Console.WriteLine("OAVGJORT");
+}
+else if (heroHp == 0)
+{
+  Console.WriteLine($"{villainName} vann!");
+}
+else
+{
+  Console.WriteLine($"{heroName} vann!");
+}
+
+Console.WriteLine("Tryck på vilken knapp du vill för att fortsätta.");
+Console.ReadKey();
